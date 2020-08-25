311ac5fee425999d416a0a4a5f0d1b81b4844d97
Choicelin <413807584@qq.com>
2020-08-25 16:26:05 +0800

    save

---
+++ part4/for_case.py
@@ -0,0 +1,10 @@
+junk_foods = ['maidanglao', 'kendeji', 'hualaishi']
+for food in junk_foods:
+    print food
+    print 'I relly like it'
+print 'These are great!'
+
+animals = ['pig', 'duck', 'chicken']
+for animal in animals:
+    print animal
+print 'These are delicious'
